893d85f257e662414124d0bf3bf0d1dd3a02bc47
Marcio Taier <marcinhotulio@hotmail.com>
2024-09-10 09:47:59 -0300

    Project creation and NavBar design

---
+++ My-Portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "./components/NavBar";
+import { GlobalStyle } from "./styles/global";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export function App() {
+  return (
+    <>
+      <NavBar />
+      <GlobalStyle />
+    </>
+  );
+}
